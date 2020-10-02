49feb5aabbbff43087a40a029b811928d260b78d
Mmedara Josiah <mmedarajosiah@cmail.carleton.ca>
2020-10-01 20:04:43 -0400

    my buddy class models a buddy

---
+++ src/BuddyInfo.java
@@ -0,0 +1,38 @@
+
+
+public class BuddyInfo
+{
+    private String name = "";
+
+    private long phoneNumber;
+    private String birthday;
+    private String address;
+
+    public BuddyInfo(String name, long phoneNumber, String birthday, String address)
+    {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.birthday = birthday;
+        this.address = address;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public long getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public String getBirthday()
+    {
+        return birthday;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+}
